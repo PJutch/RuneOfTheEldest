1f20e221e23b0fd5c812236b0976a028dacd8f24
PJutch <103033969+PJutch@users.noreply.github.com>
2023-06-04 20:58:37 +0300

    Test Actors

---
+++ tests/AliveActor.cpp
@@ -0,0 +1,114 @@
+/* This file is part of the Rune of the Eldest.
+The Rune of the Eldest - Roguelike about the mage seeking for ancient knowledges
+Copyright (C) 2023  PJutch
+
+The Rune of the Eldest is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+The Rune of the Eldest is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with the Rune of the Eldest.
+If not, see <https://www.gnu.org/licenses/>. */
+
+#include "AliveActor.hpp"
+
+#include <gtest/gtest.h>
+
+class TestAliveActor : public AliveActor {
+public:
+	TestAliveActor(double newMaxHp, double regen_, sf::Vector3i newPosition, std::shared_ptr<World> newWorld = nullptr) :
+		AliveActor{ newMaxHp, regen_, newPosition, std::move(newWorld) } {}
+	TestAliveActor(double newMaxHp, double regen_, std::shared_ptr<World> newWorld = nullptr) :
+		AliveActor{ newMaxHp, regen_, std::move(newWorld) } {}
+
+	void nextTurn(int newNextTurn) noexcept {
+		AliveActor::nextTurn(newNextTurn);
+	}
+
+	void wait(int time) noexcept {
+		AliveActor::wait(time);
+	}
+
+	virtual void attack(Actor& actor) {}
+
+	virtual void moveSucceed() {}
+
+	void position(sf::Vector3i newPosition) noexcept {
+		AliveActor::position(newPosition);
+	}
+
+	void tryMoveTo(sf::Vector3i newPosition) {
+		AliveActor::tryMoveTo(newPosition);
+	}
+
+	void tryMoveTo(sf::Vector2i newPosition) {
+		AliveActor::tryMoveTo(newPosition);
+	}
+
+	void tryMove(sf::Vector2i offset) {
+		AliveActor::tryMoveTo(offset);
+	}
+
+	using AliveActor::hp;
+
+	void hp(double newHp) noexcept {
+		AliveActor::hp(newHp);
+	}
+
+	bool act() noexcept final {
+		return true;
+	}
+
+	bool shouldInterruptOnDelete() const noexcept final {
+		return false;
+	}
+
+	void draw(Renderer&) const noexcept final {}
+};
+
+TEST(AliveActor, initialHp) {
+	TestAliveActor actor{5.0, 1.0};
+
+	EXPECT_TRUE(actor.isAlive());
+	EXPECT_EQ(actor.hp(), 5);
+}
+
+TEST(AliveActor, beDamaged) {
+	TestAliveActor actor{ 5.0, 1.0 };
+
+	actor.beDamaged(3);
+
+	EXPECT_TRUE(actor.isAlive());
+	EXPECT_EQ(actor.hp(), 2);
+}
+
+TEST(AliveActor, beDamagedLethal) {
+	TestAliveActor actor{ 5.0, 1.0 };
+
+	actor.beDamaged(7);
+
+	EXPECT_FALSE(actor.isAlive());
+}
+
+TEST(AliveActor, regen) {
+	TestAliveActor actor{ 5.0, 1.0 };
+
+	actor.beDamaged(3);
+	actor.wait(2);
+
+	EXPECT_TRUE(actor.isAlive());
+	EXPECT_EQ(actor.hp(), 4);
+}
+
+TEST(AliveActor, regenLimit) {
+	TestAliveActor actor{ 5.0, 1.0 };
+
+	actor.beDamaged(3);
+	actor.wait(7);
+
+	EXPECT_TRUE(actor.isAlive());
+	EXPECT_EQ(actor.hp(), 5);
+}
--- tests/World.cpp
+++ tests/World.cpp
@@ -20,53 +20,163 @@ If not, see <https://www.gnu.org/licenses/>. */
 
 class TestActor : public Actor {
 public:
-	TestActor(int firstTurn, int turnDelay_, sf::Vector3i newPosition) noexcept :
-		nextTurn_{ firstTurn }, turnDelay{ turnDelay_ }, position_{ newPosition } {}
+	TestActor(sf::Vector3i newPosition, int newId = -1) noexcept :
+		position_{ newPosition }, id_{ newId } {}
+	TestActor(int firstTurn, int turnDelay_, int newId = -1, std::vector<int>* log_ = nullptr,
+		      int waitAfter_ = std::numeric_limits<int>::max(), 
+		      int dieAfter_ = std::numeric_limits<int>::max(), bool shouldInterruptOnDelete_ = false) noexcept :
+		nextTurn_{ firstTurn }, turnDelay{ turnDelay_ }, id_{ newId }, log{ log_ }, 
+		waitAfter{ waitAfter_ }, dieAfter {dieAfter_}, interruptOnDelete{ shouldInterruptOnDelete_ } {}
 
 	sf::Vector3i position() const noexcept final {
 		return position_;
 	}
 
 	bool act() override {
+		if (log)
+			log->push_back(id());
 		nextTurn_ += turnDelay;
-		return true;
+		return nextTurn_ <= waitAfter;
 	}
 
 	int nextTurn() const noexcept final {
 		return nextTurn_;
 	}
 
-	bool isAlive() const noexcept {
-		return true;
+	bool isAlive() const noexcept final {
+		return nextTurn_ <= dieAfter;
 	}
 
-	bool shouldInterruptOnDelete() const noexcept {
-		return false;
+	bool shouldInterruptOnDelete() const noexcept final {
+		return interruptOnDelete;
 	}
 
 	void beDamaged(double damage) noexcept final {}
 
 	void draw(Renderer& renderer) const noexcept final {}
-private:
-	int nextTurn_;
-	int turnDelay;
-	sf::Vector3i position_;
-};
 
-class LoggingTestActor : public TestActor {
-public:
-	LoggingTestActor(int firstTurn, int turnDelay_, sf::Vector3i newPosition, int id_, std::vector<int>& log_) noexcept :
-		TestActor{ firstTurn, turnDelay_, newPosition }, id{ id_ }, log{ &log_ } {}
-
-	bool act() {
-		log->push_back(id);
-		return TestActor::act();
+	int id() const noexcept {
+		return id_;
 	}
 private:
-	int id;
-	std::vector<int>* log;
+	int nextTurn_ = 0;
+	int turnDelay = 0;
+	sf::Vector3i position_{ 0, 0, 0 };
+
+	int id_ = -1;
+
+	std::vector<int>* log = nullptr;
+
+	int waitAfter = std::numeric_limits<int>::max();
+	int dieAfter = std::numeric_limits<int>::max();
+	bool interruptOnDelete = false;
 };
 
+TEST(World, emptyActors) {
+	World world;
+	EXPECT_TRUE(world.actors().empty());
+}
+
 TEST(World, addActor) {
-	World world{ nullptr };
+	auto actor1 = std::make_shared<TestActor>(sf::Vector3i{ 0, 0, 0 });
+	auto actor2 = std::make_shared<TestActor>(sf::Vector3i{ 2, 3, 4 });
+
+	World world;
+	world.addActor(actor1);
+	world.addActor(actor2);
+
+	EXPECT_EQ(world.actors().size(), 2);
+	EXPECT_EQ(std::ranges::count(world.actors(), actor1), 1);
+	EXPECT_EQ(std::ranges::count(world.actors(), actor2), 1);
+}
+
+TEST(World, actorAt) {
+	auto actor1 = std::make_shared<TestActor>(sf::Vector3i{ 0, 0, 0 });
+	auto actor2 = std::make_shared<TestActor>(sf::Vector3i{ 2, 3, 4 });
+
+	World world;
+	world.addActor(actor1);
+	world.addActor(actor2);
+
+	EXPECT_EQ(world.actorAt({2, 3, 4}), actor2);
+}
+
+TEST(World, actorAtNull) {
+	World world;
+	world.addActor(std::make_shared<TestActor>(sf::Vector3i{ 0, 0, 0 }));
+	world.addActor(std::make_shared<TestActor>(sf::Vector3i{ 2, 3, 4 }));
+
+	EXPECT_EQ(world.actorAt({ 2, 3, 11 }), nullptr);
+}
+
+TEST(World, update) {
+	std::vector<int> log;
+
+	World world;
+	world.addActor(std::make_shared<TestActor>(0, 2, 0,  &log));
+	world.addActor(std::make_shared<TestActor>(7, 2, 1, &log, 7));
+
+	world.update();
+
+	ASSERT_EQ(log.size(), 5);
+	EXPECT_EQ(log[0], 0);
+	EXPECT_EQ(log[1], 0);
+	EXPECT_EQ(log[2], 0);
+	EXPECT_EQ(log[3], 0);
+	EXPECT_EQ(log[4], 1);
+}
+
+TEST(World, updateMany) {
+	std::vector<int> log;
+
+	World world;
+	world.addActor(std::make_shared<TestActor>(0, 2, 0, &log));
+	world.addActor(std::make_shared<TestActor>(0, 2, 1, &log));
+	world.addActor(std::make_shared<TestActor>(0, 2, 2, &log));
+	world.addActor(std::make_shared<TestActor>(0, 2, 3, &log));
+	world.addActor(std::make_shared<TestActor>(7, 2, 4, &log, 7));
+
+	world.update();
+
+	ASSERT_EQ(log.size(), 17);
+	EXPECT_EQ(log[16], 4);
+	EXPECT_EQ(std::ranges::count(log, 0), 4);
+	EXPECT_EQ(std::ranges::count(log, 1), 4);
+	EXPECT_EQ(std::ranges::count(log, 2), 4);
+	EXPECT_EQ(std::ranges::count(log, 3), 4);
+}
+
+TEST(World, updateEmpty) {
+	std::vector<int> log;
+	World world;
+	world.update();
+	ASSERT_EQ(log.size(), 0);
+}
+
+TEST(World, updateDeath) {
+	std::vector<int> log;
+
+	World world;
+	world.addActor(std::make_shared<TestActor>(0, 2, 0, &log));
+	world.addActor(std::make_shared<TestActor>(0, 2, 1, &log, std::numeric_limits<int>::max(), 3));
+	world.addActor(std::make_shared<TestActor>(7, 2, 4, &log, 7));
+
+	world.update();
+
+	ASSERT_EQ(log.size(), 7);
+	EXPECT_EQ(log[6], 4);
+	EXPECT_EQ(std::ranges::count(log, 0), 4);
+	EXPECT_EQ(std::ranges::count(log, 1), 2);
+}
+
+TEST(World, updateDeathInterrupt) {
+	std::vector<int> log;
+
+	World world;
+	world.addActor(std::make_shared<TestActor>(0, 2, 0, &log));
+	world.addActor(std::make_shared<TestActor>(0, 2, 1, &log, std::numeric_limits<int>::max(), 3, true));
+
+	world.update();
+
+	EXPECT_EQ(std::ranges::count(log, 1), 2);
 }
