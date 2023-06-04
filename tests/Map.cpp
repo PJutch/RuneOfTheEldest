93608cd3324a43234c14dc553c1c4f14cd221612
PJutch <103033969+PJutch@users.noreply.github.com>
2023-06-04 13:59:05 +0300

    Add technical stuff tests

---
+++ tests/Map.cpp
@@ -0,0 +1,78 @@
+/* This file is part of the Rune of the Eldest.
+The Rune of the Eldest - Roguelike about the mage seeking for ancient knowledges
+Copyright (C) 2023  PJutch
+
+The Rune of the Eldest is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+The Rune of the Eldest is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with the Rune of the Eldest.
+If not, see <https://www.gnu.org/licenses/>. */
+
+#include "Map.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(Map, getPtr) {
+    UnorderedMap<int, int> map{
+        {0, 1},
+        {2, 5},
+        {3, 7}
+    };
+
+    EXPECT_EQ(getPtr(map, 2), &map[2]);
+}
+
+TEST(Map, getPtrNull) {
+    UnorderedMap<int, int> map{
+        {0, 1},
+        {2, 5},
+        {3, 7}
+    };
+
+    EXPECT_EQ(getPtr(map, 1), nullptr);
+}
+
+TEST(Map, getPtrConst) {
+    const UnorderedMap<int, int> map{
+        {0, 1},
+        {2, 5},
+        {3, 7}
+    };
+
+    EXPECT_EQ(getPtr(map, 2), &map.at(2));
+}
+
+TEST(Map, getPtrConstNull) {
+    const UnorderedMap<int, int> map{
+        {0, 1},
+        {2, 5},
+        {3, 7}
+    };
+
+    EXPECT_EQ(getPtr(map, 1), nullptr);
+}
+
+TEST(Map, getOptional) {
+    UnorderedMap<int, int> map{
+        {0, 1},
+        {2, 5},
+        {3, 7}
+    };
+
+    EXPECT_EQ(*getOptional(map, 2), 5);
+}
+
+TEST(Map, getOptionalNull) {
+    UnorderedMap<int, int> map{
+        {0, 1},
+        {2, 5},
+        {3, 7}
+    };
+
+    EXPECT_FALSE(getOptional(map, 1));
+}
