bbe7e9bcdd818fc0dcdc216e3cb40537b3b95a0a
PJutch <103033969+PJutch@users.noreply.github.com>
2023-07-05 18:38:39 +0300

    Add parse utilities

---
+++ src/util/parse.hpp
@@ -0,0 +1,157 @@
+/* This file is part of the Rune of the Eldest.
+The Rune of the Eldest - Roguelike about the mage seeking for ancient knowledges
+Copyright (C) 2023  PJutch
+
+The Rune of the Eldest is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+The Rune of the Eldest is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with the Rune of the Eldest.
+If not, see <https://www.gnu.org/licenses/>. */
+
+#ifndef PARSE_HPP_
+#define PARSE_HPP_
+
+#include "Exception.hpp"
+
+#include <format>
+#include <algorithm>
+#include <string_view>
+#include <cctype>
+
+namespace util {
+	/// More safe wrapper around isspace
+	bool isSpace(char c) noexcept {
+		return std::isspace(static_cast<unsigned char>(c));
+	}
+
+	/// More safe wrapper around isdigit
+	bool isDigit(char c) noexcept {
+		return std::isdigit(static_cast<unsigned char>(c));
+	}
+
+	/// @brief Strip spaces from left and right
+	/// @details Removes all chars satisfying isSpace from left and right.
+	/// Returns subview of input view
+	std::string_view strip(std::string_view s) noexcept {
+		auto begin = std::ranges::find_if_not(s, &isSpace);
+		if (begin == s.end())
+			return {};
+
+		auto end = std::ranges::find_if_not(s.rbegin(), s.rend(), &isSpace).base();
+		return { begin, end };
+	}
+
+	/// Error while parsing smth
+	class ParseError : public RuntimeError {
+	public:
+		ParseError(const std::string& message, std::string_view parsed, Stacktrace currentStacktrace = {}) noexcept :
+			RuntimeError{ std::format("Error while parsing \"{}\": {}", parsed, message), std::move(currentStacktrace)} {}
+	};
+
+	/// Unexpected empty string
+	class EmptyStringError : public ParseError {
+	public:
+		EmptyStringError(std::string_view parsed, Stacktrace currentStacktrace = {}) noexcept :
+			ParseError{ "Unexpected empty string", parsed, std::move(currentStacktrace) } {}
+	};
+
+	/// Char requiered to be digit
+	class NotADigitError : public ParseError {
+	public:
+		NotADigitError(char c, std::string_view parsed, Stacktrace currentStacktrace = {}) noexcept :
+			ParseError{ std::format("'{}' is not a digit", c), parsed, std::move(currentStacktrace)} {}
+	};
+
+	/// @brief Parses s as a nonnegative integer without sign
+	/// @tparam ResultT Type of the result. Not nesessary unsigned or integer.
+	/// @warning Expects that all chars in s represents the number (no trailing spaices and etc.)
+	/// @throws EmptyStringError when s is empty
+	/// @throws NotADigitError if encountered nondigit char
+	template <typename ResultT>
+	ResultT parseUint(std::string_view s) {
+		if (s.empty())
+			throw EmptyStringError{s};
+
+		ResultT result{};
+		for (char c : s) {
+			if (!isDigit(c))
+				throw NotADigitError{ c, s };
+
+			result *= 10;
+			result += (c - '0');
+		}
+
+		return result;
+	}
+
+	/// @brief Parses s as a integer with optional sign
+	/// @tparam ResultT Type of the result. Not nesessary integer.
+	/// @warning Expects that all chars in s represents the number (no trailing spaices and etc.)
+	/// @throws EmptyStringError when s is empty
+	/// @throws NotADigitError if encountered wrong char
+	template <typename ResultT>
+	ResultT parseInt(std::string_view s) {
+		if (s.empty())
+			throw EmptyStringError{s};
+
+		if (s.front() == '-') {
+			s.remove_prefix(1);
+			return -parseUint<ResultT>(s);
+		}
+
+		if (s.front() == '+')
+			s.remove_prefix(1);
+		return parseUint<ResultT>(s);
+	}
+
+	/// @brief Parses s as a real number in format [+-]?\d*.?\d*(e\d+)?
+	/// @warning Expects that all chars in s represents the number (no trailing spaices and etc.)
+	/// @throws EmptyStringError when s is empty
+	/// @throws NotADigitError if encountered wrong char
+	template <typename ResultT>
+	ResultT parseReal(std::string_view s) {
+		if (s.empty())
+			throw EmptyStringError{ s };
+
+		if (s.front() == '-') {
+			s.remove_prefix(1);
+			return -parseReal<ResultT>(s);
+		}
+
+		if (s.front() == '+')
+			s.remove_prefix(1);
+
+		using namespace std::literals;
+
+		auto nextPart = std::ranges::find_first_of(s, ".eE"sv);
+		ResultT result = (nextPart != s.begin()) ? parseUint<ResultT>({ s.begin(), nextPart }) : ResultT{};
+
+		if (nextPart != s.end() && *nextPart == '.') {
+			const static std::string exponentDelimiters = "eE";
+			auto fractionalEnd = std::ranges::find_first_of(nextPart, s.end(), exponentDelimiters.begin(), exponentDelimiters.end());
+			std::string_view fractionalStr{ std::next(nextPart), fractionalEnd };
+			nextPart = fractionalEnd;
+
+			ResultT digitValue{ 1 };
+			for (char c : fractionalStr) {
+				if (!isDigit(c))
+					throw NotADigitError{ c, s };
+
+				digitValue /= 10;
+				result += digitValue * (c - '0');
+			}
+		}
+
+		if (nextPart != s.end())
+			result *= std::pow(10, parseInt<ResultT>({ std::next(nextPart), s.end() }));
+
+		return result;
+	}
+}
+
+#endif
---
+++ tests/parse.cpp
@@ -0,0 +1,75 @@
+/* This file is part of the Rune of the Eldest.
+The Rune of the Eldest - Roguelike about the mage seeking for ancient knowledges
+Copyright (C) 2023  PJutch
+
+The Rune of the Eldest is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+The Rune of the Eldest is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with the Rune of the Eldest.
+If not, see <https://www.gnu.org/licenses/>. */
+
+#include "util/parse.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(parse, isSpace) {
+	EXPECT_TRUE(util::isSpace(' '));
+	EXPECT_TRUE(util::isSpace('\n'));
+	EXPECT_FALSE(util::isSpace('c'));
+	EXPECT_FALSE(util::isSpace('5'));
+	EXPECT_FALSE(util::isSpace(','));
+}
+
+TEST(parse, isDigit) {
+	EXPECT_FALSE(util::isDigit(' '));
+	EXPECT_FALSE(util::isDigit('\n'));
+	EXPECT_FALSE(util::isDigit('c'));
+	EXPECT_TRUE(util::isDigit('5'));
+	EXPECT_FALSE(util::isDigit(','));
+}
+
+TEST(parse, strip) {
+	using namespace std::literals;
+	EXPECT_EQ(util::strip(" \nabc "), "abc"sv);
+	EXPECT_EQ(util::strip("abc"), "abc"sv);
+	EXPECT_EQ(util::strip("  "), ""sv);
+}
+
+TEST(parse, parseUint) {
+	EXPECT_EQ(util::parseUint<int>("34"), 34);
+	EXPECT_THROW(util::parseUint<int>(""), util::EmptyStringError);
+	EXPECT_THROW(util::parseUint<int>("f"), util::NotADigitError);
+}
+
+TEST(parse, parseInt) {
+	EXPECT_EQ(util::parseInt<int>("34"), 34);
+	EXPECT_EQ(util::parseInt<int>("+34"), 34);
+	EXPECT_EQ(util::parseInt<int>("-15"), -15);
+	EXPECT_THROW(util::parseInt<int>(""), util::EmptyStringError);
+	EXPECT_THROW(util::parseInt<int>("f"), util::NotADigitError);
+}
+
+TEST(parse, parseReal) {
+	EXPECT_EQ(util::parseReal<double>("34"), 34);
+	EXPECT_EQ(util::parseReal<double>("+34"), 34);
+	EXPECT_EQ(util::parseReal<double>("-15"), -15);
+	EXPECT_EQ(util::parseReal<double>("12.7"), 12.7);
+	EXPECT_EQ(util::parseReal<double>("+12.7"), 12.7);
+	EXPECT_EQ(util::parseReal<double>("-12.7"), -12.7);
+	EXPECT_EQ(util::parseReal<double>(".5"), 0.5);
+	EXPECT_EQ(util::parseReal<double>("0.01"), 0.01);
+	EXPECT_EQ(util::parseReal<double>("1e3"), 1000);
+	EXPECT_EQ(util::parseReal<double>("1e-3"), 0.001);
+	EXPECT_EQ(util::parseReal<double>("1E3"), 1000);
+	EXPECT_EQ(util::parseReal<double>("1.2e1"), 12);
+	EXPECT_THROW(util::parseReal<double>(""), util::EmptyStringError);
+	EXPECT_THROW(util::parseReal<double>("f"), util::NotADigitError);
+	EXPECT_THROW(util::parseReal<double>("1.+"), util::NotADigitError);
+	EXPECT_THROW(util::parseReal<double>("1e"), util::EmptyStringError);
+}
+
